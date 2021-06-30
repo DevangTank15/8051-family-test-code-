9c10c0d82b36cb5c556d33e0f065a31eb552bbfe
Devang Tank <tank_devang@yahoo.in>
2021-07-01 02:30:24 +0530

    Added Struct to Transfer Byte into Character
    -> Added Structure which Convert Byte data into appropriate bits

--- include/main.h
+++ include/main.h
@@ -1,1 +1,17 @@
+#include <stdint.h>
+
 #define PIN_D0  P1
+
+typedef union {
+    struct __attribute__((Packed)) {
+        unsigned bit0:1;
+        unsigned bit1:1;
+        unsigned bit2:1;
+        unsigned bit3:1;
+        unsigned bit4:1;
+        unsigned bit5:1;
+        unsigned bit6:1;
+        unsigned bit7:1;
+    };
+    uint8_t all;
+} BytetoBitstr;
